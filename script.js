2b41a839b9448fb5f3612999b4495e79af6bb6ca
arod1104 <rodriguez.alex1828@gmail.com>
2024-08-10 19:01:13 -0500

    Added grid with color changing sqiuares, opacity decreases by 10% but the squares dont get darker

---
+++ script.js
@@ -0,0 +1,59 @@
+const CONTAINER_WIDTH = 720;
+const CONTAINER_HEIGHT = 720;
+
+const container = document.querySelector(".container");
+
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function fillContainer(dimension) {
+  for (let i = 0; i < dimension * dimension; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = `${100 / dimension}%`;
+    square.style.height = `${100 / dimension}%`;
+    square.style.display = "flex";
+    square.style.border = "1px solid black";
+    square.style.backgroundColor = "white";
+
+    square.addEventListener("mouseover", () => {
+      if (square.style.backgroundColor === "white") {
+        square.style.backgroundColor = getRandomColor();
+        square.style.opacity = "1"; // Set initial opacity to 100%
+      } else {
+        let currentOpacity = parseFloat(square.style.opacity);
+        if (currentOpacity > 0) {
+          square.style.opacity = (currentOpacity - 0.1).toString();
+        }
+      }
+    });
+    container.appendChild(square);
+  }
+}
+
+fillContainer(16);
+
+const resizeButton = document.querySelector("#resize");
+resizeButton.addEventListener("click", () => {
+  const dimension = prompt("Enter the new dimension (1-100):");
+  if (dimension < 1 || dimension > 100) {
+    alert("Invalid dimension");
+    return;
+  } else {
+    container.innerHTML = "";
+    fillContainer(dimension);
+  }
+});
+
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener("click", () => {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.style.backgroundColor = "white";
+    square.style.opacity = "1";
+  });
+});
